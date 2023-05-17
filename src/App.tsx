865b299b50fc07406ece36fc4c20217d84b9a663
Egor Ermolaev <egor.ermolaev@uni.lu>
2023-05-17 16:24:35 +0600

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="bgimg">
+                <div className="topleft">
+                    <p>Coming soon</p>
+                </div>
+                <div className="middle">
+                    <h1>GoEat</h1>
+                    <hr/>
+                    <a href="https://t.me/go_eat"><p>Join Telegram channel</p></a>
+                </div>
+                <div className="bottomleft">
+                    <p>Sabrina Kuisi</p>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
